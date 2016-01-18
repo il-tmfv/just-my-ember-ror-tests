17d38be41c41a9d58bf781c0a567a8d523c0fe2c
Ilya Timofeev <il.tmfv@gmail.com>
2016-01-18 20:08:06 +0300

    Add route to ember app in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+    mount_ember_app :frontend, to: "/"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
